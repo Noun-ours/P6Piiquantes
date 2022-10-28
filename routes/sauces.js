11b4608e228ca4b76ee77da07e230996766a4335
Noun-ours <yannaplein@gmail.com>
2022-10-28 17:38:45 +0200

    creation du P6

---
+++ routes/sauces.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+
+
+
+
+const multer = require('../middleware/multer-config');
+// const multer = require('multer');
+
+const saucesCtrl = require('../controllers/sauces');
+
+router.post('/',auth,multer,saucesCtrl.createSauce );
+  
+  router.get('/:id',auth, saucesCtrl.getOneSauce);
+  
+  router.put('/:id',auth,multer,saucesCtrl.modifySauce );
+  
+  router.delete('/:id',auth,saucesCtrl.deleteSauce );
+  
+  router.use('/',saucesCtrl.getAllSauce );
+  
+
+module.exports = router;
